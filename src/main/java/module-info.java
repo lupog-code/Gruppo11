c98d1e9c68dc9585375c791126c2ae0a7380185a
lupog-code <gabri.lupo374@gmail.com>
2024-12-04 15:20:51 +0100

    fix

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.rubrica {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.rubrica to javafx.fxml;
-    exports com.mycompany.rubrica;
-}
-*/
